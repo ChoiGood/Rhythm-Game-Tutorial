961066fc83f3f78e10cf41263cf315b1be64124d
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2026-01-01 21:03:27 +0900

    feat : Note 기본 구조 구현
    - Note 프리팹 생성
    - Note.cs 작성
    - Note 판정을 위한 UI 생성
    - NoteManager에서 Instantiate
    - (테스트) CenterFlame.cs 에서 BGM 재생

---
+++ Assets/Scripts/Manager/Note.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float noteSpeed = 400;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+}
